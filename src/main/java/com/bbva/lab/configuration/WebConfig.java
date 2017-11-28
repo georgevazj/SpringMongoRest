39ee868cf8920ff81699e17124f4fc54435080ed
Jorge <georgevazj@gmail.com>
2017-11-28 18:52:29 +0100

    Se incluye configuracion en Web MVC para habilitar origenes CORS

---
+++ src/main/java/com/bbva/lab/configuration/WebConfig.java
@@ -0,0 +1,20 @@
+package com.bbva.lab.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Jorge on 28/11/17.
+ */
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
